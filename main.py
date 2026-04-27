85ace25e02f59857ea84be37b6f5fe6046c15739
bruno <brunodcozac@usp.br>
2026-04-26 21:12:01 -0300

    plot melhorado

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def derivada_segunda(x,h,func):
 def solução_analitica(x,y):
     return np.exp(-y)*np.sin(2*np.pi*x)
 
+constante=4*np.pi**2-3
 h1=0.1
 h2=0.05
 h3=0.025
@@ -25,17 +26,15 @@ malha_aprox3=np.zeros((40,80))
 malha_aprox4=np.zeros((80,160))
 malha_aprox5=np.zeros((160,320))
 
-erro1=np.zeros((10,20))
-erro2=np.zeros((20,40))
-erro3=np.zeros((40,80))
-erro4=np.zeros((80,160))
-erro5=np.zeros((160,320))
 malhas_exatas = {}
-
+erros={}
 for h in H:
+    #criando a malha
     x=np.linspace(0,1,int(1/h))
     y = np.linspace(-1, 1, int(2 / h))
     X,Y=np.meshgrid(x,y)
+
+
     malhas_exatas[h]=solução_analitica(X,Y)
 
 
@@ -43,6 +42,5 @@ for h in H:
     fig, (ax,ax2) = plt.subplots(ncols=2,subplot_kw={"projection": "3d"})
 
     ax.plot_surface(X, Y, malhas_exatas[h], cmap=cm.jet, linewidth=0, antialiased=False)
-
 
     plt.show()
